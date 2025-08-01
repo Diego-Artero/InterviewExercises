26b58efbe5adce46519a4fad52353ae0c95b0530
Diego-Artero <diego.bernardo.artero@gmail.com>
2025-08-01 11:42:07 -0300

    Added the data cleaning script && changed the config.yaml to support it

--- src/etl/transform/etl_transform.py
+++ src/etl/transform/etl_transform.py
@@ -15,6 +15,29 @@ def load_config():
 config = load_config()
 
 # Diretórios
+
+CLEANED_DIR = os.path.join(BASE_DIR, config["scraping"]["save_path_processed_cleaned"])
 FORMATTED_DIR = os.path.join(BASE_DIR, config["scraping"]["save_path_processed_formatted"])
+PESSOAS_DIR = os.path.join(FORMATTED_DIR, 'pessoas.csv')
+SINISTROS_DIR = os.path.join(FORMATTED_DIR, 'sinistros.csv')
+VEICULOS_DIR = os.path.join(FORMATTED_DIR, 'veiculos.csv')
+DIRS = [PESSOAS_DIR,SINISTROS_DIR,VEICULOS_DIR]
 
+for DIR in DIRS:
+    if DIR == PESSOAS_DIR:
+        df = pd.read_csv(DIR)
+        df.columns = df.columns.str.lower().str.strip().str.replace(' ', '_')
+        df = df[df['gravidade_lesao'] != 'NÃO DISPONIVEL']
 
+    elif DIR == VEICULOS_DIR:
+        df = pd.read_csv(DIR)
+        df.columns = df.columns.str.lower().str.strip().str.replace(' ', '_')
+        df = df[df['tipo_veiculo'] != 'NAO DISPONIVEL']
+
+    else:
+        df = pd.read_csv(DIR)
+        df.columns = df.columns.str.lower().str.strip().str.replace(' ', '_')
+        df = df[df['tipo_registro'] != 'NOTIFICACAO']    
+
+    output_file = os.path.join(CLEANED_DIR, os.path.basename(DIR))
+    df.to_csv(output_file, index=False)

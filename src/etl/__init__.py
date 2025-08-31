6485dd0c7d2ba652885d6e9151a18cdea7ece41e
Diego-Artero <diego.bernardo.artero@gmail.com>
2025-08-31 18:42:22 -0300

    Added notebooks directory

--- src/etl/__init__.py
+++ src/etl/__init__.py
@@ -2,13 +2,14 @@
 Pacote ETL — Ponto de entrada para extração, transformação e carregamento dos dados do Infosiga.
 """
 
-from .extract import extract_infosiga_data
-from .transform import clean_infosiga_data
-from .load import save_dataframes_to_sqlite, process_and_load_data
+from .extract import extract_zip, parse_report, download_latest_report
+from .transform import data_cleaner
+from .load import process_and_load_data
 
 __all__ = [
-    "extract_infosiga_data",
-    "clean_infosiga_data",
-    "save_dataframes_to_sqlite",
-    "process_and_load_data",
+    "extract_zip",
+    "parse_report",
+    "download_latest_report",
+    "data_cleaner",
+    "process_and_load_data"
 ]
